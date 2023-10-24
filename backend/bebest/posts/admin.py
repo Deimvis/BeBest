1f4a81ce3acf684ac94486ba40bcff7dc2f099ee
Dmitriy Brusenin <star_maps@mail.ru>
2023-10-25 00:17:06 +0300

    update admin page for posts

--- backend/bebest/posts/admin.py
+++ backend/bebest/posts/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'published', 'canonized_url', 'rank', 'source_name')
+
+    @admin.display(ordering="publish_timestamp")
+    def published(self, obj: Post):
+        from datetime import datetime
+        return datetime.fromtimestamp(obj.publish_timestamp)
